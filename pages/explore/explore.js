2769a2507ad7ca7a062689e52549903b41a85265
uniquexiaobai <unique_xiaobai@163.com>
2016-10-03 14:28:30 +0800

    add picker in explore and famous

--- pages/explore/explore.js
+++ pages/explore/explore.js
@@ -1,5 +1,20 @@
 
 var app = getApp()
 Page({
-  
+  data: {
+    languageArray: ['All Language', 'C', 'CSS', 'Go', 'HTML', 'Java', 'JavaScript', 'Lua', 'Object-C', 'Perl', 'PHP', 'Python', 'R', 'Ruby', 'Scala', 'Shell', 'Swift'],
+    languageIndex: 0,
+    tabArray: ['daily', 'weekly', 'monthly'],
+    tabIndex: 0
+  },
+  bindLanguagePickerChange: function(e) {
+    this.setData({
+      languageIndex: e.detail.value
+    });
+  },
+  bindTabPickerChange: function(e) {
+    this.setData({
+      tabIndex: e.detail.value
+    });
+  }
 });
--- pages/famous/famous.js
+++ pages/famous/famous.js
@@ -1,5 +1,20 @@
 
 var app = getApp()
 Page({
-
+  data: {
+    locationArray: ['Australia', 'China', 'Canada', 'France', 'Germany', 'India', 'Japan', 'UK', 'USA'],
+    locationIndex: 0,
+    languageArray: ['All Language', 'ActionScript', 'C', 'C#', 'C++', 'Clojure', 'CoffeeScript', 'CSS', 'Go', 'Haskell', 'HTML', 'Java', 'JavaScript', 'Lua', 'Matlab', 'Object-C', 'Perl', 'PHP', 'Python', 'R', 'Ruby', 'Scala', 'Shell', 'Swift'],
+    languageIndex: 0
+  },
+  bindLocationPickerChange: function(e) {
+    this.setData({
+      locationIndex: e.detail.value
+    });
+  },
+  bindLanguagePickerChange: function(e) {
+    this.setData({
+      languageIndex: e.detail.value
+    });
+  }
 });
