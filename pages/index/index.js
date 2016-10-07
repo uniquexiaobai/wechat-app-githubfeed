a32a3afdac973a14e7fe8ef63d0e310e441e2fff
uniquexiaobai <unique_xiaobai@163.com>
2016-10-07 22:07:35 +0800

    signin finished

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,7 @@
 //index.js
 //获取应用实例
-var app = getApp()
+var app = getApp();
+
 Page({
   data: {
     motto: 'Feed',
@@ -20,11 +21,5 @@ Page({
   onLoad: function () {
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
   }
 });
--- pages/signin/signin.js
+++ pages/signin/signin.js
@@ -1,4 +1,12 @@
 
+var app = getApp();
+
 Page({
-  
+  handleSigninSubmit(e) {
+    var user = { username: e.detail.value.username };
+    wx.setStorageSync('user', user);
+    wx.navigateTo({
+      url: '../index/index'
+    });
+  }
 });
