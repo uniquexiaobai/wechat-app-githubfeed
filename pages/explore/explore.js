bedf6818db629413ae63c0f7e38a686ca41757c4
uniquexiaobai <unique_xiaobai@163.com>
2016-10-09 20:26:52 +0800

    add bindscrolltoupper handler

--- pages/explore/explore.js
+++ pages/explore/explore.js
@@ -6,50 +6,58 @@ const basic_url = 'http://trending.codehub-app.com/v2/trending?since=';
 
 Page({
   data: {
-    languageArray: ['All Language', 'HTML', 'CSS', 'JavaScript', 'C', 'Java', 'PHP', 'Python', 'Ruby', 'Swift'],
+    languageArray: ['All Language', 'CSS', 'JavaScript', 'C', 'Java', 'PHP', 'Python', 'Ruby', 'Swift'],
     languageIndex: 0,
     tabArray: ['daily', 'weekly', 'monthly'],
     tabIndex: 0,
     loading_hidden: true
   },
 
-  onLoad(options) {
-    this.setData({
-      loading_hidden: false
-    });
-    const url = basic_url + this.data.tabArray[this.data.tabIndex];
-    this.fetchReposData(url);
+  onLoad (options) {
+    this.refreshData();
   },
 
-  fetchReposData(url) {
+  _reloadUrl () {
+    const basic_url = 'http://trending.codehub-app.com/v2/trending?since=';
+    if (this.data.languageIndex === 0) {
+      return url = basic_url + this.data.tabArray[this.data.tabIndex];
+    }
+    return `${basic_url}${this.data.tabArray[this.data.tabIndex]}&language=${this.data.languageArray[this.data.languageIndex].toLowerCase()}`;
+  },
+
+  fetchReposData (url) {
     services.fetch(url).then(res => {
+      if (res.data) {
+        this.setData({
+          items: res.data
+        });
+      }
       this.setData({
-        items: res.data,
         loading_hidden: true
       });
     });
   },
 
-  handleLanguagePickerChange(e) {
+  handleLanguagePickerChange (e) {
     this.setData({
       languageIndex: e.detail.value,
       loading_hidden: false
     });
-    let url = '';
-    if (this.data.languageIndex === 0) {
-      url = basic_url + this.data.tabArray[this.data.tabIndex];
-    } else {
-      url = `${basic_url}${this.data.tabArray[this.data.tabIndex]}&language=${this.data.languageArray[this.data.languageIndex].toLowerCase()}`;
-    }
-    this.fetchReposData(url);
+    this.fetchReposData(this._reloadUrl());
   },
 
-  handleTabPickerChange(e) {
+  handleTabPickerChange (e) {
     this.setData({
       tabIndex: e.detail.value,
       loading_hidden: false
     });
-    const url = basic_url + this.data.tabArray[this.data.tabIndex];
-    this.fetchReposData(url);
+    this.fetchReposData(this._reloadUrl());
+  },
+
+  refreshData () {
+    this.setData({
+      loading_hidden: false
+    });
+    this.fetchReposData(this._reloadUrl());
   }
 });
--- pages/famous/famous.js
+++ pages/famous/famous.js
@@ -2,59 +2,92 @@
 const app = getApp();
 const services = require('../../utils/services.js');
 
-const basic_url = 'https://api.github.com/search/users?q=';
-
 Page({
   data: {
-    locationArray: ['World', 'Australia', 'China', 'Japan', 'USA'],
-    locationIndex: 0,
-    languageArray: ['All Language', 'C', 'CSS', 'Java', 'JavaScript', 'PHP', 'Python', 'Swift'],
+    locationArray: ['All Country', 'China', 'USA', 'Japan', 'Australia'],
+    locationIndex: 1,
+    languageArray: ['All Language', 'CSS', 'Java', 'JavaScript', 'PHP', 'Python', 'Swift', 'C'],
     languageIndex: 0,
-    loading_hidden: true
+    loading_hidden: true,
+    items: [],
+    page: 1,
+    incomplete_results: false
   },
 
-  onLoad(options) {
+  onLoad () {
+    this.refreshData();
+  },
+
+  _reloadUrl () {
+    const basic_url = 'https://api.github.com/search/users?q=';
+    if (!this.data.locationIndex) {
+      return `${basic_url}language:${this.data.languageArray[this.data.languageIndex]}&sort=followers&page=${this.data.page}`;
+    }
+    if (!this.data.languageIndex) {
+      return `${basic_url}location:${this.data.locationArray[this.data.locationIndex]}&sort=followers&page=${this.data.page}`;
+    }
+    return `${basic_url}location:${this.data.locationArray[this.data.locationIndex]}+language:${this.data.languageArray[this.data.languageIndex]}&sort=followers&page=${this.data.page}`;
+  },
+
+  _initData () {
     this.setData({
-      loading_hidden: false
+      items: [],
+      page: 1,
+      incomplete_results: false
     });
-
-    const url = `${basic_url}language:${this.data.languageArray[this.data.languageIndex]}&sort=followers&page=1`;
-    this.fetchUsersData(url);
   },
 
-  fetchUsersData(url) {
+  fetchUsersData (url) {
     services.fetch(url).then(res => {
+      if (res.data.items) {
+        this.setData({
+          items: this.data.items.concat(res.data.items),
+          page: 1,
+          incomplete_results: res.data.incomplete_results
+        });
+      }
       this.setData({
-        items: res.data.items,
         loading_hidden: true
       });
     });
   },
 
-  handleLocationPickerChange(e) {
-    this.setData({
-      locationIndex: e.detail.value,
-      loading_hidden: false
-    });
-    let url = '';
-    if (this.data.locationIndex === 0) {
-      url = `${basic_url}language:${this.data.languageArray[this.data.languageIndex]}&sort=followers&page=1`;
-    } else {
-      url = `${basic_url}location:${this.data.locationArray[this.data.locationIndex]}+language:${this.data.languageArray[this.data.languageIndex]}&sort=followers&page=1`;
+  handleLocationPickerChange (e) {
+    if (this.data.languageIndex || e.detail.value) {
+      this.setData({
+        locationIndex: e.detail.value,
+        loading_hidden: false
+      });
+      this._initData();
+      this.fetchUsersData(this._reloadUrl());
     }
-    this.fetchUsersData(url);
   },
 
-  handleLanguagePickerChange(e) {
+  handleLanguagePickerChange (e) {
+    if (this.data.locationIndex || e.detail.value) {
+      this.setData({
+        languageIndex: e.detail.value,
+        loading_hidden: false
+      });
+      this._initData();
+      this.fetchUsersData(this._reloadUrl());
+    }
+  },
+
+  loadMoreData () {
+    if (this.data.incomplete_results) return;
     this.setData({
-      languageIndex: e.detail.value,
+      loading_hidden: false,
+      page: ++this.data.page
+    });
+    this.fetchUsersData(this._reloadUrl());
+  },
+
+  refreshData () {
+    this.setData({
       loading_hidden: false
     });
-    const url = `${basic_url}location:${this.data.locationArray[this.data.locationIndex]}+language:${this.data.languageArray[this.data.languageIndex]}&sort=followers&page=1`;
-    this.fetchUsersData(url);
-  },
-
-  loadMore() {
-    console.log('loadMore');
+    this._initData();
+    this.fetchUsersData(this._reloadUrl());
   }
 });
