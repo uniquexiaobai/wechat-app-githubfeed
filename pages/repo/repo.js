894372743ab393c9efd13fe8fdc58a09582b6235
uniquexiaobai <unique_xiaobai@163.com>
2016-10-11 09:37:42 +0800

    add repo page

---
+++ pages/repo/repo.js
@@ -0,0 +1,35 @@
+const services = require('../../utils/services.js');
+
+Page({
+  data: {
+    loading_hidden: true
+  },
+
+  onLoad (options) {
+    this.setData({
+      repo_full_name: options.repo_full_name,
+      loading_hidden: false
+    });
+    this.fetchRepoData(this._reloadUrl());
+  },
+
+  _reloadUrl () {
+    const basic_url = 'https://api.github.com/repos/';
+    return basic_url + this.data.repo_full_name;
+  },
+
+  fetchRepoData (url) {
+    services.fetch(url).then(res => {
+      this.setData({
+        repo: res.data,
+        loading_hidden: true
+      });
+    });
+  },
+
+  onReady () {
+    wx.setNavigationBarTitle({
+      title: this.data.repo_full_name
+    });
+  }
+});
